bd0c297662f5b2922bd12d0ee58362159f52d239
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2026-02-15 23:33:10 -0300

    Add:refresh useFetch hook when url updates

--- hook_app/src/03-examples/MultipleCustomHooks.jsx
+++ hook_app/src/03-examples/MultipleCustomHooks.jsx
@@ -3,9 +3,17 @@ import { useFetch } from '../hooks/useFetch'
 
 export const MultipleCustomHooks = () => {
     
-    useFetch();
+   const { data, isLoading, hasError } = useFetch('https://pokeapi.co/api/v2/pokemon/647');
 
     return (
-        <div>MultipleCustomHooks</div>
+        <>
+            <h1>Información del Pokémon</h1>
+            <hr />
+
+            {isLoading && <p>Cargando...</p>}
+
+            {/* <pre>{JSON.stringify(data,null,2)}</pre> */}
+            <h2>{data?.species.name}</h2>
+        </>
     )
 }
--- hook_app/src/hooks/useFetch.js
+++ hook_app/src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { use, useState } from "react"
 
-export const useFetch = () => {
+export const useFetch = ( url ) => {
   
     const [state, setState] = useState({
         data: null,
@@ -15,11 +15,46 @@ export const useFetch = () => {
     //   return () => {
     //     second
     //   }
-    }, []);
+    }, [url]);
+
+    const setLoadingState = () => {
+        setState({
+            data:null,
+            isLoading:true,
+            hasError:false,
+            error:null,
+        });
+    };
 
     const getFetch = async () => {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/647');
+
+        setLoadingState();
+        
+        const response = await fetch(url);//'https://pokeapi.co/api/v2/pokemon/647'
+
+        //sleep
+        await new Promise (resolve=> setTimeout(resolve,2000));
+
+        if(!response.ok) {
+            setState({
+                data: null,
+                isLoading: false,
+                hasError: true,
+                errorMessage: {
+                    code: response.status,
+                    message: response.statusText,
+                },
+            });
+            return;
+        };
+
         const data = await response.json();
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            errorMessage: null,
+        });
         console.log({data}); 
     };
     
@@ -28,6 +63,6 @@ export const useFetch = () => {
         data: state.data,
         isLoading: state.isLoading,
         hasError: state.hasError,
-    }
+    };
 
 }
