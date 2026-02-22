a55d05e571a0baeecca623c1227db76e3b54056d
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2026-02-22 18:03:09 -0300

    Add: useCallback Hook

--- hook_app/src/main.jsx
+++ hook_app/src/main.jsx
@@ -10,7 +10,8 @@ import './index.css'
 // import { FocusScreen } from './04-useRef/FocusScreen.jsx'
 // import { Layout } from './05-useLayoutEffect/Layout.jsx'
 // import { Memorize } from './06-memos/Memorize'
-import { MemorHook } from './06-memos/MemoHook'
+// import { MemorHook } from './06-memos/MemoHook'
+import { CallbackHook } from './06-memos/CallbackHook'
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     // {/* <HooksApp /> */}
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')).render(
         // <FocusScreen />
         // <Layout />
         // <Memorize />
-        <MemorHook />
+        // <MemorHook />
+        <CallbackHook />
   // </StrictMode>,
 )
