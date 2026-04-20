e1b002b25f45d27d69b1ebc53836b6e16e90f0d8
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2026-04-19 21:52:40 -0300

    add: todo list app layout

--- hook_app/src/08-useReducer/TodoApp.jsx
+++ hook_app/src/08-useReducer/TodoApp.jsx
@@ -8,7 +8,7 @@ const initialState = [
     },
     {
         id: new Date().getTime()*3,
-        description: 'Recolectar la piedra del alma',
+        description: 'Recolectar la piedra del tiempo',
         done: false
     },
 ];
@@ -18,19 +18,44 @@ export const TodoApp = () => {
     const [todos, dispatch] = useReducer(todoReducer, initialState);   
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>TodoApp 10, <small>pendientes:2</small></h1>
             <hr />
-            <ul>
-                <li>
-                    Item1
-                </li>
-                <li>
-                    Item2
-                </li>
-                <li>
-                    Item3
-                </li>
-            </ul>
+
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group">
+                        {
+                            todos.map(todo => (
+                                <li className="list-group-item d-flex justify-content-between">
+                                    <span className="align-self-center">Item 1</span>
+                                    <button className="btn btn-danger ">
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                        
+                    </ul>
+                </div>
+
+                <div className="col-5">
+                    <h4>
+                        Agregar TODO
+                    </h4>
+                    <hr />
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="¿Que hay que hacer?"
+                            className="form-control"
+                        />  
+                        <button type="submit"
+                            className="btn btn-outline-primary mt-1 ">
+                            Argegar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
