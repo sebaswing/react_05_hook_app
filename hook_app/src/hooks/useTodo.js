6c7394ea85b5a5d3a605d536b0188c1c00add7ed
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2026-07-01 15:30:20 -0300

    Adding useTodo Hook, and counters for number of tasks

--- hook_app/src/hooks/useTodo.js
+++ hook_app/src/hooks/useTodo.js
@@ -1,5 +1,55 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
 export const useTodo = (todo) => {
+
+    //  const initialState = [];
+
+    const init =() => {
+        return JSON.parse(localStorage.getItem('todos')) || [];
+    }  
+
+    //TODO crear un custom hook para el todoApp DONE
+    //exponer todos, handleNewTodo, handleDeleteTodo, handleToggleTodo DONE
+    //
+    const [todos, dispatch] = useReducer(todoReducer, [],init);  
   
 
+    useEffect(() => {
+        localStorage.setItem('todos',JSON.stringify(todos));
+    }, [todos]);
 
-}
+    const handleNewTodo=(todo)=>{
+        console.log({todo});
+        const action = {
+            type:'[TODO] Add Todo',
+            payload: todo,
+        };
+        dispatch(action);
+    }
+
+    const handleDeleteTodo=(id)=>{
+        // console.log({id});
+        dispatch({
+            type:'[TODO] Remove Todo',
+            payload: id,
+        });
+    }
+
+    const handleToggleTodo=(id)=>{
+        // console.log({id});
+        dispatch({
+            type:'[TODO] Toggle Todo',
+            payload: id,
+        });
+    }
+
+    return{
+        todos,
+        // ...todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+} 
+
