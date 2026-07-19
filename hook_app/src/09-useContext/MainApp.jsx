b7a7bbeb7c78624f29422f1c82e268b8585612fc
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2026-07-19 18:58:01 -0300

    add navbar and navegation features

--- hook_app/src/09-useContext/MainApp.jsx
+++ hook_app/src/09-useContext/MainApp.jsx
@@ -7,7 +7,7 @@ import { NavBar } from './NavBar'
 export const MainApp = () => {
   return (
     <>
-        <h1> MainApp</h1>
+        {/* <h1> MainApp</h1> */}
           <NavBar/>
         <hr/>    
 
