34f0d192ae856725f1629a5cf899799d0a04651d
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2026-08-02 18:09:18 -0300

    Adding context for user

--- hook_app/src/09-useContext/LoginPage.jsx
+++ hook_app/src/09-useContext/LoginPage.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './context/UserContext'
 
 export const LoginPage = () => {
+
+  const {user,setUser} = useContext(UserContext); // se desestructura el user del context, para poder usarlo en el componente
+  console.log(user);
+
   return (
     <>
         <h1>LoginPage</h1>
         <hr/>    
+
+        <pre>
+          {JSON.stringify(user,null,3)}
+        </pre>
+
+        <button 
+          className='btn btn-primary'
+          onClick={ ()=> 
+            setUser ({id:123, name:'Sebastian Be',email:'Sebastian@mail.com' })
+            }
+        >
+          Establecer usuario
+        </button>
     </>
   )
 }
--- hook_app/src/09-useContext/context/UserProvider.jsx
+++ hook_app/src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = { 
-    id:123,
-    name: 'Sebastián B',
-    email: 'SebastianB@googl.com'
-}
+// const user = { 
+//     id:123,
+//     name: 'Sebastián B',
+//     email: 'SebastianB@googl.com'
+// }
 
 export const UserProvider = ( { children } ) => {
+
+  const [user, setUser] = useState();
+
   return (
-    <UserContext.Provider value={{hola: 'Mundo',user:user}} >
+    <UserContext.Provider value={{user,setUser}} >
       {children}
     </UserContext.Provider>
   )
